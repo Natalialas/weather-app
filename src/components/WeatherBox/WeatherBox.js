cb4be5e2e03f62f61da1ce68c5bc54d4c2f384fc
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-01-11 13:21:58 +0100

    Finish

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,13 +1,60 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
-const WeatherBox = props => {
+const WeatherBox = () => {
+
+  const [weatherData, setWeatherData] = useState('');
+  const [pending, setPending] = useState(false);
+  const [handleError, setHandleError] = useState(false);
+
+  const handleCityChange = useCallback((city) => {
+
+    setWeatherData('');
+    setPending(true);
+    setHandleError(false);
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=187c7fbdf54f1fdc9931e2f733daaa8f&units=metric`)
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json()
+          .then((data) => {
+
+    setPending(false);
+
+    const newData = {
+      city: data.name,
+      temp: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].main
+    };
+
+    setWeatherData(newData);
+    console.log('WeatherData', newData);
+   });
+
+  } else {
+    setHandleError(true);
+  }
+  });
+
+  }, []);
+
   return (
     <section>
-      <PickCity />
-      <WeatherSummary />
-      <Loader />
+      <PickCity action={handleCityChange} />
+      {pending && !weatherData && !handleError &&  <Loader />}
+
+      {weatherData && <WeatherSummary
+          city={weatherData.city}
+          temp={weatherData.temp}
+          icon={weatherData.icon}
+          description={weatherData.description}
+        />
+      }
+      {handleError &&  <ErrorBox />}
     </section>
   )
 };
